483ad37378960d55e45124f7465a255883e633d0
Ashutosh Adhao <79149856+ashutoshadhao@users.noreply.github.com>
2022-09-04 07:34:29 +0530

    Time: 0 ms (100.00%), Space: 5.7 MB (100.00%) - LeetHub

--- 2396-strictly-palindromic-number/2396-strictly-palindromic-number.cpp
+++ 2396-strictly-palindromic-number/2396-strictly-palindromic-number.cpp
@@ -3,14 +3,6 @@ class Solution
     public:
         bool isStrictlyPalindromic(int n)
         {
-            for (int i = 2; i <= n - 2; i++)
-            {
-                string s = bitset< 64 >( n << i ).to_string();
-                string rev = s;
-                reverse(rev.begin(), rev.end());
-                // cout << rev << "   " << s << endl;
-                if (s != rev) return false;
-            }
-            return true;
+            return false ;
         }
 };
