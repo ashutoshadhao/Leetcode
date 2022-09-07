815a55d79c9713cf230a4365670c4894ca3759c1
Ashutosh Adhao <79149856+ashutoshadhao@users.noreply.github.com>
2022-09-07 09:02:44 +0530

    Time: 18 ms (5.61%), Space: 6.6 MB (16.06%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.cpp
+++ 392-is-subsequence/392-is-subsequence.cpp
@@ -12,7 +12,18 @@ public:
     }
     bool isSubsequence(string s, string t) {
         int n = s.size() , m = t.size();
-        vector<vector<int>> dp(n,vector<int>(m,-1));
-        return topdown(s,t,0,0,dp);
+        vector<vector<bool>> dp(n+1,vector<bool>(m+1,0));
+        for(int i = n ; i >= 0 ; i-- )
+        {
+            for(int j = m ; j >= 0 ; j-- )
+            {
+                if( i == s.size() ) dp[i][j] =  true;
+               else if( j == t.size() ) dp[i][j] =  false ;
+               else if( s[i] == t[j] ) 
+                     dp[i][j] =dp[i+1][j+1];
+               else  dp[i][j] = dp[i][j+1];
+            }
+        }
+        return dp[0][0];
     }
 };
