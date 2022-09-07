e2923d7e3d523cb2276e117201caaf28b58b765d
Ashutosh Adhao <79149856+ashutoshadhao@users.noreply.github.com>
2022-09-07 08:58:07 +0530

    Time: 142 ms (5.02%), Space: 282.2 MB (5.07%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.cpp
+++ 392-is-subsequence/392-is-subsequence.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
-    bool topdown( string s, string t ,int i , int j )
+    bool topdown( string s, string t ,int i , int j , vector<vector<int>> &dp)
     {
-        // if( i == s.size() and j == t.size() )
-        //     return true ;
+        
         if( i == s.size() ) return true;
         if( j == t.size() ) return false ;
+        if(dp[i][j]!=-1) return dp[i][j];
         if( s[i] == t[j] ) 
-            return topdown(s,t,i+1,j+1);
-        return topdown(s,t,i,j+1);
+            return dp[i][j] = topdown(s,t,i+1,j+1,dp);
+        return dp[i][j] = topdown(s,t,i,j+1,dp);
     }
     bool isSubsequence(string s, string t) {
         int n = s.size() , m = t.size();
         vector<vector<int>> dp(n,vector<int>(m,-1));
-        return topdown(s,t,0,0);
+        return topdown(s,t,0,0,dp);
     }
 };
