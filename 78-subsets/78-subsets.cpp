de29447da67f07a74c9d927dc9b3537bd98c769f
Ashutosh Adhao <79149856+ashutoshadhao@users.noreply.github.com>
2022-09-13 20:37:04 +0530

    Time: 8 ms (26.90%), Space: 7.7 MB (47.51%) - LeetHub

---
+++ 78-subsets/78-subsets.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void topdown( vector<int> &nums, set<vector<int>> &ans ,int i , vector<int> &res )
+    {
+        if( i == nums.size() )
+        {
+            ans.insert(res);
+            return ; 
+        }
+        res.push_back(nums[i]);
+        topdown(nums,ans,i+1,res);
+        res.pop_back();
+        topdown(nums,ans,i+1,res);
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        set<vector<int>> s ; 
+        vector<int> res; 
+        topdown(nums,s,0,res );
+        vector<vector<int>> ans; 
+        for(auto it : s )
+        {
+            ans.push_back(it);
+        }
+        return ans; 
+    }
+};
