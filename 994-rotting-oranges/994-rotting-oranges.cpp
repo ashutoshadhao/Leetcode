4eecc5f85e02f68b625ca47cef0c96f0679e34a0
Ashutosh Adhao <79149856+ashutoshadhao@users.noreply.github.com>
2022-10-03 16:42:31 +0530

    Time: 24 ms (10.07%), Space: 13.8 MB (12.82%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.cpp
+++ 994-rotting-oranges/994-rotting-oranges.cpp
@@ -1,48 +1,48 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int n = grid.size() , m = grid[0].size() , freshcnt = 0 ; 
-        vector<vector<int>> vis(n,vector<int>(m,0));
-        queue<vector<int>> q; 
-        for(int i = 0 ; i < n ; i++ )
+        queue<vector<int>> q ;
+        int freashOranges = 0 , n = grid.size() , m = grid[0].size() ; 
+        vector<vector<int>> vis(n,vector<int>(m , 0 )) ;
+        for( int i = 0 ; i < n ; i++ )
         {
-            for(int j = 0 ; j < m ; j++ )
+            for( int j = 0 ;  j< m ;j++)
             {
-                if(grid[i][j] == 2 )
+                if( grid[i][j] ==2 )
                 {
                     vis[i][j] = 2 ;
                     q.push({i,j,0});
                 }
-                if(grid[i][j] == 1 ) freshcnt++ ; 
+                else if( grid[i][j] )  freashOranges++ ;
+                    
             }
         }
-        int totalTime = 0 ;
-        int deltaRow[] = {-1,0,+1,0};
-        int deltaCol[] = {0,1,0,-1};
-        int cnt = 0 ; 
+        int DR[] = { 0, -1, 0 , 1 } ;
+        int DC[] ={-1, 0 , +1 , 0};
+        int cnt = 0 ,  maxiTime = 0 ; 
         while(!q.empty())
         {
-            auto vec = q.front() ; 
+            vector<int> node = q.front() ;
+            int row = node[0] ;
+            int col = node[1] ;
+            int time = node[2] ;
             q.pop() ;
-            int row = vec[0];
-            int col = vec[1] ;
-            int time = vec[2];
-            totalTime = max(totalTime , time );
-            for(int i = 0 ; i < 4 ; i++ )
+            maxiTime = max(maxiTime, time ) ;
+            for( int i = 0 ; i < 4 ; i++ )
             {
-                int nrow = row + deltaRow[i] ;
-                int ncol = col + deltaCol[i] ;
-                if( nrow >= 0 and ncol >= 0 and nrow < n and ncol < m and vis[nrow][ncol] != 2 and 
-                  grid[nrow][ncol] == 1 )
+                int nrow = row + DR[i] ; 
+                int ncol = col + DC[i] ; 
+                if( nrow >= 0 and nrow < n and ncol>=0 and ncol < m 
+                   and vis[nrow][ncol] != 2 and grid[nrow][ncol] == 1 )
                 {
-                    q.push({nrow,ncol,time+1});
                     vis[nrow][ncol] = 2 ;
-                    cnt++;
+                    q.push({nrow,ncol,time+1}) ; 
+                    cnt++ ;
                 }
             }
         }
-        if(cnt != freshcnt ) return -1 ;
-        return totalTime ;
-       
+        if( cnt != freashOranges ) return -1 ;
+        return maxiTime ;
+        
     }
 };
