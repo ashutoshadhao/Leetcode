bea562f591ab71d1cd6b34593eeceaf78a6596ab
Ashutosh Adhao <79149856+ashutoshadhao@users.noreply.github.com>
2022-10-08 00:43:26 +0530

    Time: 340 ms (5.18%), Space: 64.5 MB (92.26%) - LeetHub

--- 938-range-sum-of-bst/938-range-sum-of-bst.cpp
+++ 938-range-sum-of-bst/938-range-sum-of-bst.cpp
@@ -1,12 +1,12 @@
 /**
  *Definition for a binary tree node.
  *struct TreeNode {
- *  int val;
- *  TreeNode * left;
- *  TreeNode * right;
- *  TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *int val;
+ *TreeNode * left;
+ *TreeNode * right;
+ *TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  *};
  */
 class Solution
@@ -17,8 +17,10 @@ class Solution
             if (root == NULL) return;
             if (root->val >= low and root->val <= high)
                 sum += root->val;
-            preorder(root->left, low, high, sum);
-            preorder(root->right, low, high, sum);
+            if (low <= root->val)
+                preorder(root->left, low, high, sum);
+            if (high >= root->val)
+                preorder(root->right, low, high, sum);
         }
     int rangeSumBST(TreeNode *root, int low, int high)
     {
