63722fcb159c48be0ba50fa21d20c5c650201c54
Ashutosh Adhao <79149856+ashutoshadhao@users.noreply.github.com>
2022-11-04 09:00:03 +0530

    Time: 22 ms (26.57%), Space: 7.8 MB (61.66%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
@@ -1,39 +1,38 @@
 class Solution
 {
+    bool check(char ch)
+    {
+        char it = ch ;
+        if (it == 'a'
+            or it == 'e'
+            or it == 'i'
+            or it == 'o'
+            or it == 'u'
+            or it == 'A'
+            or it == 'E'
+            or it == 'I'
+            or it == 'O'
+            or it == 'U')
+            return true;
+        return false;
+    }
     public:
         string reverseVowels(string s)
         {
             string ch;
-            for (auto it: s)
+            int i = 0, j = s.size() - 1;
+            while (i < j)
             {
-                if (it == 'a'
-                    or it == 'e'
-                    or it == 'i'
-                    or it == 'o'
-                    or it == 'u'
-                    or it == 'A'
-                    or it == 'E'
-                    or it == 'I'
-                    or it == 'O'
-                    or it == 'U')
-                    ch.push_back(it);
-            }
-            reverse(ch.begin(), ch.end());
-            int j = 0;
-            for (int i = 0; i < s.size(); i++)
-            {
-                char it = s[i];
-                if (it == 'a'
-                    or it == 'e'
-                    or it == 'i'
-                    or it == 'o'
-                    or it == 'u'
-                    or it == 'A'
-                    or it == 'E'
-                    or it == 'I'
-                    or it == 'O'
-                    or it == 'U')
-                    s[i] = ch[j++];
+                char a = s[i];
+                char b = s[j];
+                if (check(a) and check(b))
+                    swap(s[i++], s[j--]);
+                else if (check(a))
+                    j--;
+                else if (check(b))
+                    i++;
+                else
+                    i++, j--;
             }
             return s;
         }
